63f51a2f5c31669324be2fbdf37ca17450f2a55b
gint367 <rulo.ginting@gmail.com>
2024-09-09 17:12:30 +0200

    added comment

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,7 +6,7 @@ import { FaBug } from "react-icons/fa";
 import classNames from 'classnames';
 
 const NavBar = () => {
-    const currentPath = usePathname();
+    const currentPath = usePathname(); //this allow us to know our current page
     //console.log(currentPath);
 
     const links = [
@@ -17,8 +17,8 @@ const NavBar = () => {
     <nav className='flex space-x-5 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><FaBug/></Link>
         <ul className='flex space-x-5'>
-            {links.map(link => 
-            <Link key={link.href} className={classNames({
+            {links.map(link => // use the const links for the item
+            <Link key={link.href} className={classNames({ // uses module classNames to organize the styles
                 'text-zinc-900': link.href === currentPath,
                 'text-zinc-500': link.href !== currentPath,
                 'hover:text-zinc-800 transition-colors': true
