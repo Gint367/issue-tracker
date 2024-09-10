ee26875fb6a7b711d2ebf3c2140fafd112754696
gint367 <rulo.ginting@gmail.com>
2024-09-10 16:16:46 +0200

    add loading Skeletons

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,19 +1,16 @@
+import { Table } from '@radix-ui/themes'
 import React from 'react'
-import { Button, Table } from '@radix-ui/themes'
-import Link from 'next/link'
-import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
-import delay from 'delay'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 import IssueActions from './issueActions'
 
-const IssuesPage = async () => {
-  const issue= await prisma.issue.findMany()
-  await delay(2000)
+const LoadingIssuePage = () => {
+    const issue = [1,2,3,4,5]
   return (
     <div>
-      
-      <IssueActions />
-      <Table.Root variant='surface'>
+        <IssueActions />
+    <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell >Issue</Table.ColumnHeaderCell>
@@ -23,25 +20,24 @@ const IssuesPage = async () => {
         </Table.Header>
         <Table.Body>
           {issue.map( issue => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell >
-                {issue.title}
+                <Skeleton/>
                 <div className='block md:hidden'>
-                  <IssueStatusBadge status={issue.status} />
+                    <Skeleton/>
                 </div>
               </Table.Cell>
               <Table.Cell className='hidden md:table-cell'>
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton/>
               </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'><Skeleton/></Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
       </Table.Root>
-      
-    
+
     </div>
   )
 }
 
-export default IssuesPage
+export default LoadingIssuePage
