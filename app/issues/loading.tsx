ab179d3168933bf0387dc49e6015d3ab662d3a83
gint367 <rulo.ginting@gmail.com>
2024-09-10 16:17:14 +0200

    fix format

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -10,31 +10,31 @@ const LoadingIssuePage = () => {
   return (
     <div>
         <IssueActions />
-    <Table.Root variant='surface'>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell >Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {issue.map( issue => (
-            <Table.Row key={issue}>
-              <Table.Cell >
-                <Skeleton/>
-                <div className='block md:hidden'>
-                    <Skeleton/>
-                </div>
-              </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>
-                <Skeleton/>
-              </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'><Skeleton/></Table.Cell>
+        <Table.Root variant='surface'>
+            <Table.Header>
+            <Table.Row>
+                <Table.ColumnHeaderCell >Issue</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+                <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
+            </Table.Header>
+            <Table.Body>
+            {issue.map( issue => (
+                <Table.Row key={issue}>
+                <Table.Cell >
+                    <Skeleton/>
+                    <div className='block md:hidden'>
+                        <Skeleton/>
+                    </div>
+                </Table.Cell>
+                <Table.Cell className='hidden md:table-cell'>
+                    <Skeleton/>
+                </Table.Cell>
+                <Table.Cell className='hidden md:table-cell'><Skeleton/></Table.Cell>
+                </Table.Row>
+            ))}
+            </Table.Body>
+        </Table.Root>
 
     </div>
   )
