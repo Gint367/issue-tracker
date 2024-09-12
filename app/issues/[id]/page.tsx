6d000c9b434dffd13f64dcdc2da6564633475ebd
gint367 <rulo.ginting@gmail.com>
2024-09-12 13:00:27 +0200

    refactor: apply single responsibility principle

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,9 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import prisma from '@/prisma/client';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import { Box, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
-import ReactMarkdown from 'react-markdown';
-import { Pencil2Icon } from '@radix-ui/react-icons'
-import Link from 'next/link';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
+
 
 interface Props {
     params: { id:string}
@@ -26,27 +25,16 @@ const IssueDetailPage = async ({ params }: Props) =>  {
     //await delay(2000)
     
     return (
-        <Grid columns={{initial: "1", md: "2" }} gap={"5"}>
+        <Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
             <Box>
-                <Heading>{issue.title} </Heading>
-                <Flex className='space-x-3' my={"2"}>
-                    <IssueStatusBadge status={issue.status}/>
-                    <Text>{issue.createdAt.toDateString()} </Text>
-                </Flex>
-                <Card className='prose' mt={"4"}>
-                <ReactMarkdown children = {issue.description} />
-                </Card>
+                <IssueDetails issue={issue} />
+
             </Box>
             <Box>
-                <Button>
-                    <Pencil2Icon/>
-                    <Link href={'/issue/${issue.id}/edit'}>
-                        Edit Issue
-                    </Link>
-                </Button>
+                <EditIssueButton issueId={issue.id} />
+
             </Box>
         </Grid>
-       
     )
 }
 
