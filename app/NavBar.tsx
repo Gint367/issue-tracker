169688b23f74986b0eb9af63dc5cf15d443f7080
gint367 <rulo.ginting@gmail.com>
2024-09-13 15:40:46 +0200

    Refactor NavBar component and add dynamic navigation links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,15 +6,12 @@ import { FaBug } from "react-icons/fa";
 import classNames from 'classnames';
 import { useSession } from "next-auth/react"
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
+import { Session } from 'next-auth';
 
 const NavBar = () => {
-    const currentPath = usePathname(); //this allow us to know our current page
     //console.log(currentPath);
     const { status, data: session } = useSession();
-    const links = [
-        { label: 'Dashboard', href: '/' },
-        { label: 'Issues', href: '/issues' },
-    ]
+    
     return (
         <nav className='border-b mb-5 px-5 py-3'>
             <Container>
@@ -22,54 +19,10 @@ const NavBar = () => {
                 <Flex justify={"between"}>
                     <Flex align={"center"} gap={"3"}>
                         <Link href="/"><FaBug /></Link>
-                        <ul className='flex space-x-5'>
-
-                            {links.map(link => // use the const links for the item
-                                <li key={link.href}>
-                                    <Link className={classNames({ // uses module classNames to organize the styles
-                                        'text-zinc-900': link.href === currentPath,
-                                        'text-zinc-500': link.href !== currentPath,
-                                        'hover:text-zinc-800 transition-colors': true
-                                    })} href={link.href}>{link.label}</Link>
-                                </li>
-                            )}
-                        </ul>
+                        <NavLinks />
+                        
                     </Flex>
-                    <Box>
-                        {
-                            status === "authenticated" && (
-                                <>
-                                    <DropdownMenu.Root>
-                                        <DropdownMenu.Trigger>
-                                            <Avatar 
-                                                src={session?.user!.image!} 
-                                                fallback="?" 
-                                                size={"2"}
-                                                radius='full'
-                                                className='cursor-pointer'
-                                                referrerPolicy='no-referrer'
-                                            />
-                                        </DropdownMenu.Trigger>
-                                        <DropdownMenu.Content>
-                                            <DropdownMenu.Label>
-                                                <Text size="2">
-                                                    {session?.user!.email}
-                                                </Text>
-                                            </DropdownMenu.Label>
-                                            <DropdownMenu.Item>
-                                                <Link href="/api/auth/signout">Log Out</Link>  
-                                            </DropdownMenu.Item> 
-                                        </DropdownMenu.Content>
-
-                                    </DropdownMenu.Root>
-                                </>
-                            )
-                        }
-                        {
-                            status === "unauthenticated" && (
-                                <Link href="/api/auth/signin" >Log In</Link>)
-                        }
-                    </Box>
+                    {NavRight(status, session)}
 
                 </Flex>
             </Container>
@@ -80,3 +33,56 @@ const NavBar = () => {
 }
 
 export default NavBar
+
+const NavLinks = () => {
+    const currentPath = usePathname(); //this allow us to know our current page
+    const links = [
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' },
+    ]
+    return <ul className='flex space-x-5'>
+
+        {links.map(link => // use the const links for the item
+            <li key={link.href}>
+                <Link className={classNames({ // uses module classNames to organize the styles
+                    "nav-link": true,
+                    '!text-zinc-900': link.href === currentPath,
+                })} href={link.href}>{link.label}</Link>
+            </li>
+        )}
+    </ul>
+}
+
+function NavRight(status: string, session: Session | null) {
+
+    if (status === "loading") return null;
+    if (status === "unauthenticated") return <Link className='nav-link' href="/api/auth/signin">Log In</Link>;
+
+    return <Box>
+        <>
+            <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                    <Avatar
+                        src={session?.user!.image!}
+                        fallback="?"
+                        size={"2"}
+                        radius='full'
+                        className='cursor-pointer'
+                        referrerPolicy='no-referrer' />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                    <DropdownMenu.Label>
+                        <Text size="2">
+                            {session?.user!.email}
+                        </Text>
+                    </DropdownMenu.Label>
+                    <DropdownMenu.Item>
+                        <Link href="/api/auth/signout">Log Out</Link>
+                    </DropdownMenu.Item>
+                </DropdownMenu.Content>
+
+            </DropdownMenu.Root>
+        </>
+
+    </Box>;
+}
