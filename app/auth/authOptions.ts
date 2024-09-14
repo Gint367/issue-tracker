6335184f83f8af42eaf00e41c1320e98a2feec1e
gint367 <rulo.ginting@gmail.com>
2024-09-15 00:37:54 +0200

    Refactor authOptions to made redirect to baseurl

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -4,7 +4,6 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const authOptions: NextAuthOptions = {
-
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
@@ -15,7 +14,13 @@ const authOptions: NextAuthOptions = {
     session: {
         strategy: 'jwt',
     },
+    callbacks: {
+        async redirect({ url, baseUrl }) {
+            TODO: 'fix bugs, the default callback is always go to /issues/new'
+            return baseUrl;
+        }
 
+    }
 }
 
 export default authOptions
