5b353c7e87c6208a359ad13dc1c5ddf2b7cc4338
gint367 <rulo.ginting@gmail.com>
2024-09-16 19:19:56 +0200

    fix avatar not loading

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -9,16 +9,16 @@ import ReactMarkdown from 'react-markdown';
 const IssueDetails = ({ issue }: { issue: Issue }) => {
     return (
         <>
-            <Heading>{issue.title} </Heading>
-            <Flex className="space-x-3" my={"2"}>
+            <Heading>{issue.title}</Heading>
+            <Flex className="space-x-3" my="2">
                 <IssueStatusBadge status={issue.status} />
-                <Text>{issue.createdAt.toDateString()} </Text>
+                <Text>{issue.createdAt.toDateString()}</Text>
             </Flex>
-            <Card className="prose max-w-full" mt={"4"}>
-                <ReactMarkdown children={issue.description} />
+            <Card className="prose max-w-full" mt="4">
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
             </Card>
         </>
     );
-}
+};
 
 export default IssueDetails
