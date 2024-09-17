adcfbb93aed4b71ed81e1a621a4104bc0a7a95f1
gint367 <rulo.ginting@gmail.com>
2024-09-17 13:08:36 +0200

    Refactor IssueSummary component to make the entire card clickable

--- app/IssueSummary.tsx
+++ app/IssueSummary.tsx
@@ -21,16 +21,15 @@ const IssueSummary = ({ open, inProgress, closed }: Props) => {
     return (
         <Flex gap={"3"}>
             {statuses.map(status => (
-                <Card key={status.status}>
-                    <Flex direction={'column'} align={'center'} gap={"1"}>
-                        <Link className='text-sm font-medium' href={`/issues?status=${status.status}`}>
-                            {status.label}
-                        </Link>
-                        <Text size={"5"} className='font-bold'>{status.value}</Text>
-                    </Flex>
-                </Card>
+                <Link key={status.status} href={`/issues?status=${status.status}`} className='w-full'>
+                    <Card>
+                        <Flex direction={'column'} align={'center'} gap={"1"}>
+                            <Text className='text-sm font-medium'>{status.label}</Text>
+                            <Text size={"5"} className='font-bold'>{status.value}</Text>
+                        </Flex>
+                    </Card>
+                </Link>
             ))}
-
         </Flex>
     )
 }
